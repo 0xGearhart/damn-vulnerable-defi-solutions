6393a78480c2af890344921f76b348cf0f3e9315
0xGearhart <geardev26@gmail.com>
2026-01-26 08:00:57 +0000

    fix comment formatting

--- src/truster/TrusterLenderPool.sol
+++ src/truster/TrusterLenderPool.sol
@@ -25,16 +25,15 @@ contract TrusterLenderPool is ReentrancyGuard {
         uint256 balanceBefore = token.balanceOf(address(this));
 
         token.transfer(borrower, amount);
-        /**
-         * @audit passing full control of the transaction to the function caller is very dangerous
-         * This next line allows anyone to call any function an any contract with this contracts address as msg.sender
-         *
-         * To solve this challenge we start by calling this function with an amount of 0 so we don't have to worry about returning any funds
-         * The real key to this vulnerability is the target and data input fields since we can encode any function selector and contract address
-         * We will use these input fields to send the DVT contract address along with the "approve(address,uint256)" function selector
-         * This allows us to approve our player address to spend an unlimited amount of DVT tokens on this contracts behalf since we will be calling with this address as msg.sender
-         * After the approval we are free to transfer the entire DVT balance of this contract wherever we want using transferFrom
-         */
+
+        // @audit passing full control of the transaction to the function caller is very dangerous
+        // This next line allows anyone to call any function an any contract with this contracts address as msg.sender
+
+        // To solve this challenge we start by calling this function with an amount of 0 so we don't have to worry about returning any funds
+        // The real key to this vulnerability is the target and data input fields since we can encode any function selector and contract address
+        // We will use these input fields to send the DVT contract address along with the "approve(address,uint256)" function selector
+        // This allows us to approve our player address to spend an unlimited amount of DVT tokens on this contracts behalf since we will be calling with this address as msg.sender
+        // After the approval we are free to transfer the entire DVT balance of this contract wherever we want using transferFrom
         target.functionCall(data);
 
         if (token.balanceOf(address(this)) < balanceBefore) {

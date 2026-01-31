87fdcff3199bfe229c1e24ba9afc7aadeaaf3b22
0xGearhart <geardev26@gmail.com>
2026-01-31 03:00:23 +0000

    add puppet challenge solution and audit comments to contract

--- src/puppet/PuppetPool.sol
+++ src/puppet/PuppetPool.sol
@@ -56,6 +56,17 @@ contract PuppetPool is ReentrancyGuard {
         return amount * _computeOraclePrice() * DEPOSIT_FACTOR / 10 ** 18;
     }
 
+    // @audit Using pools as price oracles is never recommended since they are vulnerable to price manipulation attacks
+    // Something like ChainLink price feeds is recommended as they are much more resistant to price manipulation and typically use more than one source for pricing
+    // These price feeds should also be checked for staleness and circuit breakers to verify price is recent and valid
+    // Additional oracles are also recommended in case any one should fail or become stale
+    // This is exactly what we will use to pass this challenge
+    // (From player EOA)
+    // 1) make a tokenToEthSwap on low liquidity uniswap pool that is being used as the oracle to crash the price of DVT token
+    // 2) deploy challengeSolver contract and fund with ETH to execute following transactions within constructor so that total transactions == 1
+    //      (From challengeSolver contract)
+    //      3) call borrow to receive all DVT tokens in pool at reduced price
+    //      4) send DVT tokens to recovery address
     function _computeOraclePrice() private view returns (uint256) {
         // calculates the price of the token in wei according to Uniswap pair
         return uniswapPair.balance * (10 ** 18) / token.balanceOf(uniswapPair);

214db5d5dfecfa50a09d4a645c8ec2aeee2af5e0
0xGearhart <geardev26@gmail.com>
2026-02-19 10:47:35 -0800

    initial wallet mining challenge comments and issues

--- src/wallet-mining/AuthorizerUpgradeable.sol
+++ src/wallet-mining/AuthorizerUpgradeable.sol
@@ -3,15 +3,20 @@
 pragma solidity =0.8.25;
 
 contract AuthorizerUpgradeable {
+    // @audit Upgradable contracts cannot have their state variables set in the implementation contract, they must be initialized at deployment time or through a function call to be saved in the proxy contracts storage.
     uint256 public needsInit = 1;
     mapping(address => mapping(address => uint256)) private wards;
 
     event Rely(address indexed usr, address aim);
 
+    // @audit Upgradable contracts cannot use constructors, they need to implement initialization type functions that are called at proxy deployment time
     constructor() {
         needsInit = 0; // freeze implementation
     }
 
+    // @audit Critical - Missing Access Controls / Initializer Not Disabled After Initialization
+    // This function can be called repeatedly by anyone, even though initialization is only supposed to happen once.
+    // This allows anyone to assign any ward to any aim which bypasses the intended functionality of this contract.
     function init(address[] memory _wards, address[] memory _aims) external {
         require(needsInit != 0, "cannot init");
         for (uint256 i = 0; i < _wards.length; i++) {

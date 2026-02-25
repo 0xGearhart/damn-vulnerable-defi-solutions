bab8a2852226b8bc94478b18a9cdb1adf135fee8
0xGearhart <geardev26@gmail.com>
2026-02-24 18:37:40 -0800

    fix comment accuracy

--- src/wallet-mining/AuthorizerUpgradeable.sol
+++ src/wallet-mining/AuthorizerUpgradeable.sol
@@ -3,19 +3,20 @@
 pragma solidity =0.8.25;
 
 contract AuthorizerUpgradeable {
-    // @audit Upgradable contracts cannot have their state variables set in the implementation contract, they must be initialized at deployment time or through a function call to be saved in the proxy contracts storage.
+    // @audit Upgradable contracts cannot have their state variables set in the implementation contract, they must be initialized at deployment time with a delegatecall through the proxy contract to be saved in the proxy contracts storage.
     uint256 public needsInit = 1;
     mapping(address => mapping(address => uint256)) private wards;
 
     event Rely(address indexed usr, address aim);
 
-    // @audit Upgradable contracts cannot use constructors, they need to implement initialization type functions that are called at proxy deployment time
+    // @audit Upgradable contracts cannot use constructors, they need to implement initialization type functions that are called at proxy deployment time.
+    // Otherwise, state is only set within the implementation contract instead of the proxy contract where all state should be stored.
     constructor() {
         needsInit = 0; // freeze implementation
     }
 
-    // @audit Critical - Missing Access Controls / Initializer Not Disabled After Initialization
-    // This function can be called repeatedly by anyone, even though initialization is only supposed to happen once.
+    // @audit Critical - Missing Access Controls / Initializer Not Disabled After Deployment
+    // This function can be called by anyone, even though initialization is supposedly disabled by constructor.
     // This allows anyone to assign any ward to any aim which bypasses the intended functionality of this contract.
     function init(address[] memory _wards, address[] memory _aims) external {
         require(needsInit != 0, "cannot init");
